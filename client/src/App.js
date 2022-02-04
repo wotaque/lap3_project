0b823091552d46b318754b187581c366b9e4b3ec
Marina Ivanova <90510429+wotaque@users.noreply.github.com>
2022-02-04 14:29:09 +0000

    created CRA

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+
+function App() {
+    return (
+        <div id="app">
+            <main>
+                <Switch>
+                    <Route></Route>
+                    
+                </Switch>
+            </main>
+        </div>
+    )
+}
+
+export default App;
