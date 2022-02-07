85ead3b612b2dff4088ced9031b6d2071661e09b
Marina Ivanova <90510429+wotaque@users.noreply.github.com>
2022-02-07 14:33:16 +0000

    "changes4"

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 //import { Switch, Route } from 'react-router-dom';
 import QuizStart from './components/QuizStart';
 
+
 function App() {
     return (
         <div id="app">
+
             <main>
                 <QuizStart />
             </main>
+
+
         </div>
     )
 }
