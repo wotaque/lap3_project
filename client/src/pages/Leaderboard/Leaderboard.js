07bd3b3107e646f656c3411d8a0968f7f69aaaea
brandolee15 <b_lee12@hotmail.co.uk>
2022-02-08 10:29:36 +0000

    leaderboard back btn

--- client/src/pages/Leaderboard/Leaderboard.js
+++ client/src/pages/Leaderboard/Leaderboard.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router';
 
 const Leaderboard = () => {
   const [ userScore, setUserScore ] = useState([]);  
-
+  const navigate = useNavigate();
 
   useEffect(() => {
     Axios.get('http://localhost:3001/leaderboard').then((response) => {
@@ -11,14 +13,16 @@ const Leaderboard = () => {
     })
   }, [])
   
-
+  const goHome = () => {
+    navigate('/')
+}
   return <div>
       <h1>Leaderboard</h1>
       
       {userScore.map((val, key) => {
           return <div key={key}> <h1>Username: {val.name}</h1> <h1>Score: {val.score}</h1> </div>
       })}  
-
+        <Button variant="contained" onClick={goHome}>Home</Button>
   </div>;
 };
 
