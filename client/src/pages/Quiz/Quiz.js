411097363caf60492ce6dafd2c7f94636646fd91
Marina Ivanova <90510429+wotaque@users.noreply.github.com>
2022-02-09 13:50:51 +0000

    commit -m "styling Mrna"

--- client/src/pages/Quiz/Quiz.js
+++ client/src/pages/Quiz/Quiz.js
@@ -1,27 +1,24 @@
-import { CircularProgress, Typography } from '@mui/material';
+import { CircularProgress } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import Question from '../../components/Question/Question';
-import CustomTheme from './muiTheme';
-
-
-import SupportIcon from '@mui/icons-material/Support';
-
-import { Container, ThemeProvider, Card, Typography, CssBaseline, Paper, Link} from '@mui/material';
+import { Container, CssBaseline, Paper } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 
-const Quiz = ({score,questions,setScore,number,players,inputFields}) => {
+import Link from '@mui/material/Link';
+import SupportIcon from '@mui/icons-material/Support';
+import Parent from './bgTheme'
+
+
+const Quiz = ({score,questions,setScore,number,players}) => {
 
     const [choices, setChoices] = useState()
     const [curQues , setCurQues] = useState(0)
-    const [curPlay, setCurPlay] = useState()
-
+    const [curPlay, setCurPlay] = useState(1)
      
   useEffect(()=>{
     //console.log(questions)
-    setChoices(questions && 
-      handleShuffle([questions[curQues]?.correct_answer, 
-        ...questions[curQues]?.incorrect_answers,])
-    );
+    setChoices(questions && handleShuffle([questions[curQues]?.correct_answer, ...questions[curQues]?.incorrect_answers,])
+                                          );
   },[questions,curQues]); 
   
   // useEffect(()=>{
@@ -40,85 +37,74 @@ const Quiz = ({score,questions,setScore,number,players,inputFields}) => {
       background: 'linear-gradient(45deg, #A0D2EB 30%, #D0BDF4 90%)',
       border: 0,
       borderRadius: 3,
+      marginBottom: 15, 
       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-      color: '#8458B3', 
+      color: 'white',
+      padding: '5px, 30px'
     },
   });
 
  const classes = useStyles();
 
 
- console.log(inputFields)
 
-  return <div className={classes.root} style={{ width: '100%' }} >
+
+
+
+  return <Parent className={classes.root}  >
      
-     <CssBaseline />
+    <CssBaseline />
     <Container sx={{ maxWidth: 500 }} style={{ width: 400, margin: 'auto' }}>
 
-    <Paper className={classes.paperRoot}
-      component="form"
-      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
-    >
-     
-     
-     
-      {
-          questions ?(<div>
-          {/* <div>
-          Player:{inputFields.map((inputField, index) => (
-                    <div key={index}>
-                      <Typography 
-                            name="username"
-                            value={inputField.username}
-                      />
-                    </div>
-                ))}
-              
-          </div> */}
-
-          <div>
-            Player: {inputFields.map((inputField, index) => (
-              <div key={index}>{inputField[index].username}</div>
-            ))}
-          </div>
-          <Question 
-            curQues={curQues}
-            setCurQues={setCurQues}
-            questions={questions}
-            choices={choices}
-            correct={questions[curQues]?.correct_answer}
-            score={score}
-            setScore={setScore}
-            number={number}
-            players={players}
-            curPlay={curPlay}
-            setCurPlay={setCurPlay}
-          
-          />
-
+      <Paper className={classes.paperRoot}
+        component="form"
+        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+      >
+      
+  
+      
+        {questions ?(<div>
+            <div>
+              <h1>Player:{curPlay}</h1>
             </div>
 
-          ):(<CircularProgress />)
-          }
-
-          <Link
-            href="https://www.wikipedia.org/"
+            <Question 
+              curQues={curQues}
+              setCurQues={setCurQues}
+              questions={questions}
+              choices={choices}
+              correct={questions[curQues]?.correct_answer}
+              score={score}
+              setScore={setScore}
+              number={number}
+              players={players}
+              curPlay={curPlay}
+              setCurPlay={setCurPlay}
+            
+            />
+            <Link href="https://www.wikipedia.org/"
             rel="noreferrer"
             target="_blank"
-            id="tweet-quote"
-          >
-            <SupportIcon />
+            id="tweet-quote">
+            <SupportIcon 
+            style={{fontSize: 40 }}
+            />
           </Link>
 
+          </div>
+          ):(<CircularProgress />)
 
           
+        }
 
-        </Paper>
+        
 
-    
-      </Card>
-    </div>
-  )
+      </Paper>
+   
+    </Container>
+        
+         
+  </ Parent > 
 };
-
-export default Quiz;
+    
+export default Quiz; 
